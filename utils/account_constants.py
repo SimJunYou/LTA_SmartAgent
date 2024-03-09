917cce6d9eb4465a43ed2f06902ad4eb69ff8754
Wang Zihan <77315806+zihan9485@users.noreply.github.com>
2024-03-10 00:38:29 +0800

    Add AWS functions

---
+++ utils/account_constants.py
@@ -0,0 +1,7 @@
+AMI_ID = 'ami-0110dd74ac61fe967' # Innovation Challenge AMI
+SUBNET_ID1 = 'subnet-04b4716667a1b9ba9'
+SUBNET_ID2 = 'subnet-013815c1beee630fe'
+KEY_NAME = 'prod-ec2-key1'
+SECURITY_GROUP_ID = 'sg-02d3b51498c3adf5b' # Web security group, ID of the security group you want to add the instance to
+SECURITY_GROUP_ID_RDS = 'sg-0f211b59e94201cc6' # RDS Security Group
+INSTANCE_PROFILE = 'arn:aws:iam::329121819345:instance-profile/RDS_role_for_EC2'
